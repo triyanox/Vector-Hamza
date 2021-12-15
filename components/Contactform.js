0a6cc22b9df7f04242e2cad0d601c45fad8b869d
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2021-12-16 00:18:44 +0100

    Contact

--- components/Contactform.js
+++ components/Contactform.js
@@ -42,21 +42,21 @@ const Contactform = () => {
                 }} >
 
                     <div className="flex  flex-col items-center mx-auto ">
-                        <form name="contactus" method="POST" data-netlify="true" className="  flex  flex-col items-center  "   >
+                        <form name="contactus" method="POST" data-netlify="true" className="  flex  flex-col items-center "   >
                             <input type="hidden" name="form-name" value="contactus" />
 
 
 
 
                             <span className="uppercase text-center mx-auto text-black dark:text-bright-gray-50  ">Full Name</span>
-                            <input name="cname" type="text" className="w-full text-3xl h-12  text-black  mt-2 p-2 focus:outline-none focus:shadow-outline  border-2 border-black"
+                            <input name="cname" type="text" className="w-full text-3xl h-12  text-white dark:text-bright-gray-50 mt-2 p-2 focus:outline-none focus:shadow-outline bg-black dark:bg-white shadow-xl rounded  "
                                 required />
 
 
 
 
                             <span className="mt-8 uppercase text-center mx-auto  text-black dark:text-bright-gray-50  ">Email</span>
-                            <input name="cemail" type="email" className="w-full text-3xl   h-12 text-black  mt-2 p-2  focus:outline-none focus:shadow-outline  border-2 border-black"
+                            <input name="cemail" type="email" className="w-full text-3xl   h-12 text-white dark:text-bright-gray-50  mt-2 p-2  focus:outline-none focus:shadow-outline bg-black dark:bg-white shadow-xl rounded "
                                 required />
 
 
@@ -65,7 +65,7 @@ const Contactform = () => {
 
                             <span className="mt-8 uppercase text-center mx-auto  text-black dark:text-bright-gray-50   ">Message</span>
                             <textarea name="cmessage"
-                                className="w-full text-3xl h-28   text-black  mt-2 p-2  focus:outline-none focus:shadow-outline   border-2 border-black" required></textarea>
+                                className="w-full text-3xl h-28   text-white dark:text-bright-gray-50  mt-2 p-2  focus:outline-none focus:shadow-outline bg-black dark:bg-white shadow-xl rounded " required></textarea>
 
 
 
